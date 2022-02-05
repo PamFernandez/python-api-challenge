b8e97c6310279268ed7cbe003b24c3ecbafc9941
Pam Fernandez <ppossen@gmail.com>
2022-02-04 21:18:39 -0600

    through creating a csv

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e3ccf44e02cf0a5895ed131fd26f64b2"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD_ki2K7LPz6iiqXqUB8il-f5kGRDEWK4E"
